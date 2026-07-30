36df58adc49b0669da968fe398b0f933467df85a
aimeos <aimeos@aimeos.org>
2026-07-30 21:44:12 +0000

    Implemented private file storage for access restricted pages (#277)

--- database/seeders/JournalDemo.php
+++ database/seeders/JournalDemo.php
@@ -8,11 +8,9 @@
 namespace Database\Seeders;
 
 use Aimeos\Cms\Models\Element;
-use Aimeos\Cms\Models\File;
 use Aimeos\Cms\Models\Page;
 use Aimeos\Cms\Utils;
 use Aimeos\Cms\Validation;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 
@@ -569,32 +567,12 @@ class JournalDemo extends AbstractDemo
 </svg>
 SVG;
 
-            $disk = Storage::disk( config( 'cms.disk', 'public' ) );
-            $path = rtrim( 'cms/' . $this->tenant, '/' ) . '/kontur-logo.svg';
-
-            if( !$disk->put( $path, $svg ) ) {
-                throw new \Aimeos\Cms\Exception( sprintf( 'Unable to store logo "%s"', $path ) );
-            }
-
-            $data = [
-                'mime' => 'image/svg+xml',
-                'lang' => 'en',
-                'name' => 'Kontur logo',
-                'path' => $path,
-                'previews' => ['500' => $path],
-                'description' => ['en' => 'Kontur wordmark with a gold accent line'],
-            ];
-
-            $file = File::forceCreate( $data + ['editor' => 'demo'] );
-            $version = $file->versions()->forceCreate( [
-                'lang' => 'en',
-                'data' => $data,
-                'editor' => 'demo',
-            ] );
-
-            $file->forceFill( ['latest_id' => $version->id] )->saveQuietly();
-            $file->publish( $version );
-            $this->logoFile = (string) $file->refresh()->id;
+            $this->logoFile = $this->svgFile(
+                $svg,
+                'kontur-logo.svg',
+                'Kontur logo',
+                'Kontur wordmark with a gold accent line',
+            );
         }
 
         return $this->logoFile;
--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -37,7 +37,7 @@
 
         @foreach($page->ancestorsAndSelf->reverse() as $navItem)
             @if($fileId = cms($navItem, 'config.icon.data.file.id'))
-                <link rel="icon" type="{{ cmsfile($navItem, $fileId)?->mime }}" href="{{ cmsurl(cmsfile($navItem, $fileId)?->path) }}">
+                <link rel="icon" type="{{ cmsfile($navItem, $fileId)?->mime }}" href="{{ cmsasset($navItem, cmsfile($navItem, $fileId)) }}">
                 @break
             @endif
         @endforeach
@@ -124,7 +124,7 @@
                         <a href="{{ cmsroute($nav->ancestors()->first() ?? $page) }}" title="{{ config('app.name') }}" aria-label="{{ config('app.name') }}">
                             @forelse($page->ancestorsAndSelf->reverse() as $navItem)
                                 @if($fileId = cms($navItem, 'config.logo.data.file.id'))
-                                    <img src="{{ cmsurl(cmsfile($navItem, $fileId)?->path) }}" alt="{{ config('app.name') }}">
+                                    <img src="{{ cmsasset($navItem, cmsfile($navItem, $fileId)) }}" alt="{{ config('app.name') }}">
                                     @break
                                 @endif
                             @empty
@@ -215,7 +215,7 @@
             <div class="container">
                 @foreach($page->ancestorsAndSelf->reverse() as $navItem)
                     @if($fileId = cms($navItem, 'config.logo.data.file.id'))
-                        <img class="brand" src="{{ cmsurl(cmsfile($navItem, $fileId)?->path) }}" alt="{{ config('app.name') }}">
+                        <img class="brand" src="{{ cmsasset($navItem, cmsfile($navItem, $fileId)) }}" alt="{{ config('app.name') }}">
                         @break
                     @endif
                 @endforeach
